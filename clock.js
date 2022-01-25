dca905515ff948da723029a3b05c15a49638f4be
patrick letourneux <patrick.letourneux20@gmail.com>
2022-01-25 20:55:10 +0100

    fonctionnement ok

--- clock.js
+++ clock.js
@@ -1,13 +1,39 @@
 
-var second = 0;
+// initialisation
+document.getElementById('time-mesurement').value = 0;
+var second = 6;
+
+// function 
 function chrono() {
     document.querySelector('.seconde').style.transform = `rotate(${second}deg)`;
-    document.getElementById('time-mesurement').value = second/6
-    second = second+6;
+    document.getElementById('time-mesurement').value = second / 6
+    second = second + 6;
 
 }
-chrono() 
-setInterval(chrono, 1000);
+var myIntreval;
+const start = () => {
+    myIntreval = setInterval(chrono, 1000);
+}
+const pause = () => {
+    clearInterval(myIntreval);
+}
+const reset = () => {
+    clearInterval(myIntreval);
+    document.getElementById('time-mesurement').value = 0;
+    document.querySelector('.seconde').style.transform = `rotate(0deg)`;
+    second = 6;
+}
+
+// evenements
+document.getElementById('button_start')
+    .addEventListener('click', start);
+
+document.getElementById('button_pause')
+    .addEventListener('click', pause);
+
+document.getElementById('button_reset')
+    .addEventListener('click', reset);
+
 
 
 // clock();
@@ -20,7 +46,7 @@ setInterval(chrono, 1000);
 //   const hour = hours * 30;
 //   const minute = minutes * 6;
 //   const second = seconds * 6;
-  
+
 //   document.querySelector('.heure').style.transform = `rotate(${hour}deg)`;
 
 //   document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
