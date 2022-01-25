eab9111acac5ccf4a71aaec52d7db8516d4319c5
patrick letourneux <patrick.letourneux20@gmail.com>
2022-01-25 16:45:22 +0100

    chrono in progress

---
+++ clock.js
@@ -0,0 +1,30 @@
+// clock();
+
+// function clock() {
+//   const date = new Date();
+//   const hours = ((date.getHours() + 11) % 12 + 1);
+//   const minutes = date.getMinutes();
+//   const seconds = date.getSeconds();
+//   const hour = hours * 30;
+//   const minute = minutes * 6;
+//   const second = seconds * 6;
+  
+//   document.querySelector('.heure').style.transform = `rotate(${hour}deg)`;
+
+//   document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
+
+//   document.querySelector('.seconde').style.transform = `rotate(${second}deg)`;
+// }
+
+
+
+
+
+var second = 0;
+function chrono() {
+    document.querySelector('.seconde').style.transform = `rotate(${second}deg)`;
+    second = second+6;
+
+}
+chrono() 
+setInterval(chrono, 1000);
